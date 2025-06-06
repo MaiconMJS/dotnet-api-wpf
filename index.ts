72903b64227876317aa12bd240f32f7c9625785c
MaiconMJS <newoverride@gmail.com>
2025-06-06 15:34:50 -0300

    Refatoração de API

--- index.ts
+++ index.ts
@@ -3,28 +3,46 @@ dotenv.config();
 const UDP_PORT = Number(process.env.PORT_UDP);
 const WS_PORT = Number(process.env.WS_PORT);
 import dgram from "dgram";
-import { WebSocketServer } from "ws";
+import { WebSocket, WebSocketServer } from "ws";
 
 const socket = dgram.createSocket("udp4");
 const wss = new WebSocketServer({ port: WS_PORT });
 
-let percent = "50";
+let percentString = "50";
+let percent = 50;
 
 socket.on("message", (msg, rinfo) => {
   console.log(`Mensagem recebida: ${msg} de ${rinfo.address}:${rinfo.port}`);
 
-  percent = msg.toString();
+  percentString = msg.toString().trim();
+
+  if (percentString.includes("+") || percentString.includes("-")) {
+    const numericPart = percentString.replace(/[+-]/g, "");
+
+    if (!isNaN(Number(numericPart))) {
+      percent = parseInt(numericPart, 10);
+
+      if (percentString.includes("+")) {
+        percent++;
+      } else {
+        percent--;
+      }
+      percent = Math.max(0, Math.min(percent, 100));
+    } else {
+      console.error(`⚠️ Mensagem inválida recebida: ${percentString}`);
+    }
+  }
 
   wss.clients.forEach((client: any) => {
-    if (client.readyState === 1) {
-      client.send(percent);
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(percent.toString());
     }
   });
 });
 
 wss.on("connection", (ws: any) => {
   console.log("Cliente websocket conectado.");
-  ws.send(percent);
+  ws.send(percent.toString());
 
   ws.on("close", () => {
     console.log("Cliente websocket desconectado.");
